6701f95ef17fff3d7c85fa15e58dba67a5144593
David Collom <dmcollom@gmail.com>
2019-02-09 13:23:21 -0800

    updated manual ingestion script to not break for empty tarballs

--- ingest_manual.py
+++ ingest_manual.py
@@ -16,17 +16,22 @@ def get_ztf_url(date):
 
 def read_avros(url):
     with requests.get(url, stream=True) as response:
-        with tarfile.open(fileobj=response.raw, mode='r|gz') as tar:
-            while True:
-                member = tar.next()
-                if member is None:
-                    logger.info('Done ingesting this package')
-                    break
-                with tar.extractfile(member) as f:
-                    if f:
-                        fencoded = base64.b64encode(f.read()).decode('UTF-8')
-                        do_ingest(fencoded)
-            logger.info('done sending tasks', extra={'tags': {'processed_tarball': url}})
+        try:
+            with tarfile.open(fileobj=response.raw, mode='r|gz') as tar:
+                while True:
+                    member = tar.next()
+                    if member is None:
+                        logger.info('Done ingesting this package')
+                        break
+                    with tar.extractfile(member) as f:
+                        if f:
+                            fencoded = base64.b64encode(f.read()).decode('UTF-8')
+                            do_ingest(fencoded)
+                logger.info('done sending tasks', extra={'tags': {'processed_tarfile': url}})
+        except tarfile.ReadError as e:
+            logger.info('tarfile is empty', extra={'tags': {
+                'tarfile': url
+            }})
 
 if __name__ == '__main__':
     db.create_all()
