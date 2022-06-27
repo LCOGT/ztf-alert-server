17c9eff3f9ce73232a6a6ce5628ce588df3c1b7d
Lindy Lindstrom <llindstrom@lco.global>
2022-06-27 18:39:34 +0000

    change the non_detection_id_seq to bigint
    so it can be used as the default for the new PK column
    (new_id) which is a bigint and wants to re-use the
    existing non_detection_id_seq (it it must be a bigint as well)

--- db_scripts/20220623_update_nondetection_id_bigint.sql
+++ db_scripts/20220623_update_nondetection_id_bigint.sql
@@ -1,8 +1,11 @@
 BEGIN;
+-- before we create the new bigint column (new_id) and specify the PK sequence
+-- we need to tell the sequence that it must be a bigint now..
+ALTER SEQUENCE non_detection_id_seq AS bitint;
 -- bigint default nextval() is equivalent to BIGSERIAL and we will be recycling the old sequence
 ALTER TABLE non_detection ADD COLUMN new_id bigint default nextval('non_detection_id_seq');
--- changing ownership of this sequence because we will not be able to delete the old primary
--- key while it still owns the sequence
+-- change ownership of this sequence to the new_id (the new PK)
+-- because we will not be able to delete the old primary key while it still owns the sequence
 ALTER SEQUENCE non_detection_id_seq OWNED by non_detection.new_id;
 CREATE UNIQUE INDEX non_detection_new_pkey ON non_detection USING btree(new_id);
 UPDATE non_detection SET new_id = id;
