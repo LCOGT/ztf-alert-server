14d2a156578a40489bf5b58b8930b896be0c10c0
Matt Daily <mdaily@lco.global>
2022-08-16 11:17:24 -0700

    Refactor ingestion script to use multiprocessing
    This allows us to run multiple processes to ingest days in parallel.

--- ingest_manual.py
+++ ingest_manual.py
@@ -1,14 +1,18 @@
-from ingest import do_ingest
-from ztf import db, logger
-import sys
-import requests
 import tarfile
+import sys
 import base64
 from datetime import datetime, timedelta
+import multiprocessing
+import os
+
+import requests
+
+from ingest import do_ingest
+from ztf import db, logger
 
 
 ZTF_ALERT_ARCHIVE = 'https://ztf.uw.edu/alerts/public/'
-
+NUM_WORKER_PROCESSES = os.getenv('NUM_WORKER_PROCESSES', 1)
 
 def get_ztf_url(date):
     return '{0}ztf_public_{1}.tar.gz'.format(ZTF_ALERT_ARCHIVE, datetime.strftime(date, '%Y%m%d'))
@@ -28,16 +32,23 @@ def read_avros(url):
                             fencoded = base64.b64encode(f.read()).decode('UTF-8')
                             do_ingest(fencoded)
                 logger.info('done sending tasks', extra={'tags': {'processed_tarfile': url}})
-        except tarfile.ReadError as e:
-            logger.info('tarfile is empty', extra={'tags': {
+        except Exception as e:
+            logger.error('Error ingesting tarball', extra={'tags': {
                 'tarfile': url
             }})
+
+
+def error_callback(exc):
+    logger.error(f'Got error {exc}')
+    pass
+
 
 if __name__ == '__main__':
     db.create_all()
     start_date = datetime.strptime(sys.argv[1], '%Y%m%d')
     end_date = datetime.strptime(sys.argv[2], '%Y%m%d')
-    for i in range(0, (end_date - start_date).days + 1):
-        url = get_ztf_url(start_date + timedelta(days=i))
-        read_avros(url)
+    with multiprocessing.Pool(processes=NUM_WORKER_PROCESSES) as pool:
+        for i in range(0, (end_date - start_date).days + 1):
+            url = get_ztf_url(start_date + timedelta(days=i))
+            pool.apply_async(read_avros, (url,), error_callback=error_callback)
 
